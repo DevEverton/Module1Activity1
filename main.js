6ad3baf93c8f3c499bba619c613d9067034b3f18
Everton Carneiro <Everton@MacBook-Pro-de-Everton.local>
2020-05-19 22:00:51 -0300

    Finished project

---
+++ main.js
@@ -0,0 +1,37 @@
+window.addEventListener("load", start);
+
+function start() {
+  getSlidersValues();
+}
+
+function getSlidersValues() {
+  let sliderRed = document.querySelector("#slider-red");
+  let sliderGreen = document.querySelector("#slider-green");
+  let sliderBlue = document.querySelector("#slider-blue");
+  sliderRed.addEventListener("change", slidersValueChange);
+  sliderGreen.addEventListener("change", slidersValueChange);
+  sliderBlue.addEventListener("change", slidersValueChange);
+}
+
+function slidersValueChange(event) {
+  changeTextInputValue(event.target.id, event.target.value);
+}
+
+function changeTextInputValue(slider, value) {
+  let inputRed = document.querySelector("#inputRed");
+  let inputGreen = document.querySelector("#inputGreen");
+  let inputBlue = document.querySelector("#inputBlue");
+  if (slider === "slider-red") {
+    inputRed.value = value;
+  } else if (slider === "slider-green") {
+    inputGreen.value = value;
+  } else if ((slider = "slider-blue")) {
+    inputBlue.value = value;
+  }
+  renderRGB(inputRed.value, inputGreen.value, inputBlue.value);
+}
+
+function renderRGB(r, g, b) {
+  let divBox = document.querySelector("#box");
+  divBox.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+}
